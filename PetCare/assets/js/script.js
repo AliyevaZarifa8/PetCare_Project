f9e63e4254f06b37f9c5db852610d5460d76bdd8
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-06-07 10:35:47 -0700

    upicon and functionality uploaded

--- PetCare/assets/js/script.js
+++ PetCare/assets/js/script.js
@@ -1,15 +1,22 @@
 let navbar = document.querySelector("#navbar");
-
+const upIcon = document.querySelector("#upicon");
 function scrollFun() {
   let x =
-    document.body.scrollTop > 400 || document.documentElement.scrollTop > 400;
+    document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
   if (x) {
     navbar.style.background = "#2e8b57";
+    upIcon.style.display = "block";
   } else {
     navbar.style.background = "";
+    upIcon.style.display = "none";
   }
 }
 
 window.addEventListener("scroll", function () {
   scrollFun();
+});
+
+upIcon.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
