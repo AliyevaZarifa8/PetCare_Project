bec8fafb8718167ddea177289844743a03571363
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-06-08 22:01:03 -0700

    home page compliated

--- PetCare/assets/js/script.js
+++ PetCare/assets/js/script.js
@@ -1,5 +1,7 @@
 let navbar = document.querySelector("#navbar");
 const upIcon = document.querySelector("#upicon");
+
+
 function scrollFun() {
   let x =
     document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
@@ -19,4 +21,14 @@ window.addEventListener("scroll", function () {
 upIcon.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
