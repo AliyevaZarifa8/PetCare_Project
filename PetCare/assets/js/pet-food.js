2ca88ea27230a6009b5e493c9aa7ce666af5480d
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-06-10 09:03:37 -0700

    pet-food1 sections uploaded

---
+++ PetCare/assets/js/pet-food.js
@@ -0,0 +1,47 @@
+let navbar = document.querySelector("#navbar");
+const upIcon = document.querySelector("#upicon");
+navbar.style.background = "#2e8b57";
+
+function scrollFun() {
+  let x =
+    document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
+  if (x) {
+    upIcon.style.display = "block";
+  } else {
+    upIcon.style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", function () {
+  scrollFun();
+});
+
+upIcon.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
