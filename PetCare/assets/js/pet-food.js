e182000703ecdf8a183df78ac68c6dadc58ccc4b
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-06-14 17:21:08 -0700

    js and functionalities added in pet food page

--- PetCare/assets/js/pet-food.js
+++ PetCare/assets/js/pet-food.js
@@ -1,6 +1,109 @@
 let navbar = document.querySelector("#navbar");
 const upIcon = document.querySelector("#upicon");
+const petFoods = document.querySelector("#pet-foods");
 navbar.style.background = "#2e8b57";
+
+let selectFood = document.querySelector("#select");
+let searchInp = document.querySelector("#search");
+let sortBtn = document.querySelector("#sort");
+let showMore = document.querySelector(".showmore");
+let spinnerInfo = document.querySelector("#spinner");
+spinnerInfo.style.display = "flex";
+let filterData = [];
+let getallData = [];
+let sortData = [];
+let evrData = [];
+let sort = "asc";
+let maxLen = 8;
+const foodUrl = "http://localhost:8080/dog-food";
+
+async function drawFood() {
+  const res = await axios(foodUrl);
+  const data = res.data;
+  getallData = data;
+  spinnerInfo.style.display = "none";
+  filterData =
+    filterData.length || searchInp.value
+      ? filterData.slice(0, maxLen)
+      : getallData.slice(0, maxLen);
+
+  petFoods.innerHTML = "";
+
+  filterData.forEach((element) => {
+    petFoods.innerHTML += `
+    <div class="col-lg-3">
+    <div class="card">
+      <img src="${element.photo}" alt="" />
+      <h2>Category:</h2>
+      <h2>${element.category}</h2>
+      <h3>Price: $ ${element.price}</h3>
+      <p>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-regular fa-star"></i>
+        <span>${element.starprice}</span>
+      </p>
+      <div><button>Buy Now</button></div>
+    </div>
+  </div>
+        
+        `;
+  });
+}
+drawFood();
+
+showMore.addEventListener("click", function () {
+  getallData.length > maxLen + 7
+    ? maxLen + 4
+    : (maxLen = maxLen - (maxLen - getallData.length));
+  drawFood();
+  filterData = getallData.slice(0, maxLen);
+});
+
+searchInp.addEventListener("input", function (e) {
+  spinnerInfo.style.display = "flex";
+  filterData = getallData.filter((item) => {
+    return item.category.toLowerCase().includes(e.target.value.toLowerCase());
+  });
+
+  spinnerInfo.style.display = "none";
+  drawFood();
+  getallData = filterData;
+  evrData = filterData;
+});
+
+selectFood.addEventListener("input", async function (e) {
+  spinnerInfo.style.display = "flex";
+  filterData = getallData.filter((item) => {
+    return item.category.toLowerCase().includes(e.target.value.toLowerCase());
+  });
+  spinnerInfo.style.display = "none";
+  drawFood();
+});
+
+sortBtn.addEventListener("click", function () {
+  sortData = filterData;
+  if (sort == "asc") {
+    sortBtn.innerHTML = "Sort By Asc";
+    sort = "dcs";
+    sortData.sort((a, b) => a.price - b.price);
+
+    drawFood();
+  } else if (sort == "dcs") {
+    sortBtn.innerHTML = "Sort By Dcs";
+    sort = "def";
+    sortData.sort((a, b) => b.price - a.price);
+
+    drawFood();
+  } else {
+    filterData = searchInp.value ? evrData : getallData;
+    sortBtn.innerHTML = "Sort By";
+    sort = "asc";
+    drawFood();
+  }
+});
 
 function scrollFun() {
   let x =
