020fbd46b04df0dd0ae1a2d47aadb4a2cce9714f
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-06-18 00:01:37 -0700

    slider added in services page

--- PetCare/assets/js/services.js
+++ PetCare/assets/js/services.js
@@ -4,8 +4,6 @@ const pricesDnmk = document.querySelector(".prices-dinamk");
 navbar.style.background = "#2e8b57";
 const pricesUrl = "http://localhost:8080/prices-plans";
 
-
-
 function drawPrices(array) {
   pricesDnmk.innerHTML = "";
   array.forEach((element) => {
@@ -37,8 +35,6 @@ async function getallData() {
 }
 getallData();
 
-
-
 function scrollFun() {
   let x =
     document.body.scrollTop > 200 || document.documentElement.scrollTop > 200;
@@ -57,3 +53,29 @@ upIcon.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+let slide_index = 1;
+displaySlides(slide_index);
+
+function nextSlide(n) {
+  displaySlides((slide_index += n));
+}
+
+function currentSlide(n) {
+  displaySlides((slide_index = n));
+}
+
+function displaySlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("showSlide");
+  if (n > slides.length) {
+    slide_index = 1;
+  }
+  if (n < 1) {
+    slide_index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
+}
